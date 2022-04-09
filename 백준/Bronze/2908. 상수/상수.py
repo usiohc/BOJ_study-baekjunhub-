3fc4b51ce72c4f742b4eb13bfcca4d5a6c8a0b47
usiohc <89768234+usiohc@users.noreply.github.com>
2022-04-09 19:58:40 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -3,7 +3,4 @@ num1, num2 = map(int, input().split())
 rnum1 = num1//100 + (num1//10%10)*10 + (num1%10)*100
 rnum2 = num2//100 + (num2//10%10)*10 + (num2%10)*100
 
-if rnum1 > rnum2:
-    print(rnum1)
-else:
-    print(rnum2)
+print(max(rnum1, rnum2))
