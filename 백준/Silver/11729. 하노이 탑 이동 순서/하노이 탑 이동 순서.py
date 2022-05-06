4a3cfd9a936e13300aea3584303e0bc0a38174b3
usiohc <89768234+usiohc@users.noreply.github.com>
2022-05-06 10:25:36 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 976 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,11 @@
+def hanoi_rec(n, a, b):
+    if n > 1:
+        hanoi_rec(n-1, a, 6-a-b)
+    print(a, b)
+    if n > 1:
+        hanoi_rec(n-1, 6-a-b, b)
+
+n = int(input())
+
+print((2**n)-1)
+hanoi_rec(n, 1, 3)
