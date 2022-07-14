4a0e4a66d4d9091f0e07aabd11383ef798c74bf5
usiohc <89768234+usiohc@users.noreply.github.com>
2022-07-14 15:25:52 +0900

    [Silver I] Title: 나이트의 이동, Time: 3112 ms, Memory: 221160 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+def bfs(a, b, x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        q = queue.popleft()
+        x, y = q
+
+        if graph[a][b] > 0:
+            break
+        for i in range(8):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if nx<=-1 or nx>=l or ny<=-1 or ny>=l:
+                continue
+
+            if graph[nx][ny] < graph[x][y]:
+                graph[nx][ny] = graph[x][y]+1
+                queue.append((nx, ny))
+
+                
+dx = [-1, -2,-2,-1, 1, 2, 2, 1]
+dy = [-2, -1, 1, 2,-2,-1, 1, 2]
+for _ in range(int(input())):
+    l = int(input())
+    graph = [[0] * l for _ in range(l)]
+
+    a, b = map(int, input().split())
+    x, y = map(int, input().split())
+    graph[x][y] = 1
+    bfs(a, b, x, y)
+    print(graph[a][b]-1)
