70c5af3d4e1e64a3a4c22e6caa8e299fb77c1288
usiohc <89768234+usiohc@users.noreply.github.com>
2022-08-03 17:17:56 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 196 ms, Memory: 35328 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -0,0 +1,10 @@
+import bisect
+
+for _ in range(int(input())):
+    N, M = map(int, input().split())
+    A = sorted(list(map(int, input().split())))
+    B = sorted(list(map(int, input().split())))
+    cnt = 0
+    for a in A:
+        cnt += (bisect.bisect(B, a-1))
+    print(cnt)
