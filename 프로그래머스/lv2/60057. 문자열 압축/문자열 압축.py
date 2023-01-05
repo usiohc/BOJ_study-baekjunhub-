74f8683ab1663bbd57d35dfac1cfbf25d340990d
usiohc <89768234+usiohc@users.noreply.github.com>
2023-01-05 11:11:47 +0900

    [level 2] Title: 문자열 압축, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,27 @@
+from re import findall
+
+def solution(s):
+    answer = len(s)
+    if answer == 1:
+        return answer 
+    
+    for l in range(1, len(s)+1):
+        lst = [s[i:i+l] for i in range(0, len(s), l)]
+        dic = {sl:[1] for sl in set(lst)}
+        leng = len(s)
+        
+        for i in range(len(lst)-1):
+            if lst[i] == lst[i+1]:
+                leng -= l
+                dic[lst[i]][-1] += 1
+            else:
+                if dic[lst[i]][-1] > 1:
+                    dic[lst[i]].append(1)
+                    
+        for v_lst in dic.values():
+            for v in v_lst:
+                if v > 1:
+                    leng += len(str(v))
+        answer = min(answer, leng)
+
+    return answer
