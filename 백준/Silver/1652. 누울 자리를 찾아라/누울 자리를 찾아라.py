58f8395a5ad1978571aff3ad25bea7578cf018e7
usiohc <89768234+usiohc@users.noreply.github.com>
2023-02-15 17:32:56 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -0,0 +1,25 @@
+n = int(input())
+li = []
+r, c = 0, 0
+cnt = 0
+for i in range(n):
+    li.append(list(input()))
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if li[i][j] == '.':
+            cnt += 1
+        else:
+            cnt = 0
+        if cnt == 2:
+            r += 1
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if li[j][i] == '.':
+            cnt += 1
+        else:
+            cnt = 0
+        if cnt == 2:
+            c += 1
+print(r, c)
