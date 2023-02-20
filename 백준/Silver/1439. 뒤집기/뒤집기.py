57226553ce3d8d2b7921f721aad6a16f13522701
usiohc <89768234+usiohc@users.noreply.github.com>
2023-02-20 16:49:30 +0900

    [Silver V] Title: 뒤집기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,6 +1,11 @@
 s = list(map(int, input()))
-cnt = 0
+
+lst = [s[0]]
+
 for i in range(1, len(s)):
-    if s[i-1] != s[i]:
-        cnt +=1
-print((cnt+1) // 2)
+    if lst[-1] != s[i]:
+        lst.append(s[i])
+if len(lst)==1:
+    print(0)
+else:
+    print(min(lst.count(1), lst.count(0)))
