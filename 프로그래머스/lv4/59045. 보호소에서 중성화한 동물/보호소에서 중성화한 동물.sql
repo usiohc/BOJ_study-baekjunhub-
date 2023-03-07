7bf82dfcac84c6a5ae4daca37dcf173a72620d1f
usiohc <89768234+usiohc@users.noreply.github.com>
2023-03-07 15:47:02 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,6 @@
+select a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME
+from animal_ins a, animal_outs b
+where a.animal_id = b.animal_id and
+    a.sex_upon_intake like 'Intact%' and
+    REGEXP_LIKE(b.sex_upon_outcome, '^Spayed|^Neutered')
+order by a.animal_id
