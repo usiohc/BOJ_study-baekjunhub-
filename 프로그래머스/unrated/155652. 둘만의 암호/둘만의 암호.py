9c68373b75a71ece9a92f6817edf13f76f3081ed
usiohc <89768234+usiohc@users.noreply.github.com>
2023-03-12 17:52:46 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,6 @@
+def solution(s, skip, index):
+    answer = ''
+    arr = [chr(i) for i in range(97, 123) if chr(i) not in skip] * 10
+    for i in s:
+        answer += arr[arr.index(i) + index]
+    return answer
