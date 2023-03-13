7469adffdb9bff3fdc56ec0ddc30127d52a9fbf8
usiohc <89768234+usiohc@users.noreply.github.com>
2023-03-13 16:03:36 +0900

    [Gold V] Title: 치킨 배달, Time: 348 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,29 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+def solution(n, m):
+    houses, comb = [], []
+
+    for i in range(n):
+        ip = list(map(int, input().split()))
+        for j in range(n):
+            if ip[j] == 2:
+                comb.append((i, j))
+            elif ip[j] == 1:
+                houses.append((i, j))
+    
+    result = 1e4
+    for cb in combinations(comb, m):
+        dist = 0
+        for x, y in houses:
+            d = 1e4
+            for xx, yy in cb:
+                d = min(d, abs(xx-x)+abs(yy-y))
+            dist += d
+        result = min(result, dist)
+    return result
+
+
+N, M = map(int, input().split())
+print(solution(N, M))
