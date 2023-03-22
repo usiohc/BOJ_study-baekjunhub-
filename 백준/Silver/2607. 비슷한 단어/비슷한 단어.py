44bffed2e0e802b6555c9307cbae91da5a083f05
usiohc <89768234+usiohc@users.noreply.github.com>
2023-03-22 20:25:12 +0900

    [Silver III] Title: 비슷한 단어, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -0,0 +1,19 @@
+n = int(input())
+target = list(input())
+answer = 0
+
+for _ in range(n-1):
+    compare = target[:] 
+    word = input()
+    cnt = 0
+
+    for w in word:
+        if w in compare:
+            compare.remove(w)
+        else:
+            cnt += 1
+
+    if cnt < 2 and len(compare) < 2:
+        answer += 1
+
+print(answer)
